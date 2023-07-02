991b9618ed201aca76a666ad57aff67abf929574
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-07-02 00:55:52 -0700

    deployment fix

--- pages/division.js
+++ pages/division.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Navbar from "@/components/Nav";
+
 
 export default function Division() {
     return (
         <>
-            <Navbar />
-            hi
+
         </>
     )
 }
